3dc88133ae5e8177d0d87312670781a2f113f89a
Kateryna Rostovtseva <141571972+KaterynaRostovtseva@users.noreply.github.com>
2026-08-20 16:00:13 +0300

    chore: remove dist directory from git

--- dist/src/modules/auth/auth.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class AuthService {
-    validateUser(username: string, password: string): Promise<any>;
-}
--- dist/src/modules/users/users.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class UsersService {
-    findAll(): any[];
-}
