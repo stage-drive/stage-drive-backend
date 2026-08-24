ecde22bac5da7eeb4a9ca6d15700999b055f4dae
arstrong <arsendacenko89@gmail.com>
2026-08-24 12:10:53 +0300

    Implemented RefreshTocken and PasswordResetToken in db

---
+++ prisma/migrations/20260824090756_add_auth_tokens_and_enums/migration.sql
@@ -0,0 +1,65 @@
+/*
+  Warnings:
+
+  - The `status` column on the `organizations` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `status` column on the `users` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('INVITED', 'ACTIVE', 'BLOCKED', 'ARCHIVED');
+
+-- CreateEnum
+CREATE TYPE "OrganizationStatus" AS ENUM ('ACTIVE', 'BLOCKED');
+
+-- AlterTable
+ALTER TABLE "organizations" DROP COLUMN "status",
+ADD COLUMN     "status" "OrganizationStatus" NOT NULL DEFAULT 'ACTIVE';
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "last_login_at" TIMESTAMP(3),
+DROP COLUMN "status",
+ADD COLUMN     "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE';
+
+-- CreateTable
+CREATE TABLE "refresh_token" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "token_hash" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "revoked_at" TIMESTAMP(3),
+    "ip_address" TEXT,
+    "user_agent" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "refresh_token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "password_reset_tokens" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "token_hash" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "used_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "password_reset_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_token_token_hash_key" ON "refresh_token"("token_hash");
+
+-- CreateIndex
+CREATE INDEX "refresh_token_user_id_idx" ON "refresh_token"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "password_reset_tokens_token_hash_key" ON "password_reset_tokens"("token_hash");
+
+-- CreateIndex
+CREATE INDEX "password_reset_tokens_user_id_idx" ON "password_reset_tokens"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "refresh_token" ADD CONSTRAINT "refresh_token_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "password_reset_tokens" ADD CONSTRAINT "password_reset_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
