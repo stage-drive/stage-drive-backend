7c81591666001d0dbc42779fe7412450c735669e
bogdansolomich <bogdansolomich@gmail.com>
2026-09-02 13:52:11 +0300

    feat: урли для БД для тесту

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -4,3 +4,5 @@ import { join } from 'path';
 config({ path: join(__dirname, '..', '.env') });
 
 process.env.AUTH_SECRET ??= 'test-auth-secret';
+process.env.DATABASE_URL ??=
+  'postgresql://placeholder:placeholder@localhost:5432/placeholder';
